788b7a3ce800f853facd8982d9739ad4503f870c
chansmann <92978756+chansmann@users.noreply.github.com>
2022-05-22 16:02:01 +0200

    Webshop that works with APIs.
    Implementation of a standalone webshop simulator.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Webshop_simulator
  Simulates a webshop and sends requests to specific APIs.
+
+Run "webshop_simulator.py" to generate customers and orders. The server has to run and you will need extended VPN-access of the university of offenburg.
---
+++ webshop_simulator.py
@@ -0,0 +1,65 @@
+import requests
+from random import randint, choice
+import json
+
+
+from fake_data_generator import gen_customer, gen_order
+
+
+
+URL = 'http://127.0.0.1:8000/'
+API = {
+    'create_customer' : 'savetokunde',
+    'create_order' : 'api/createOrder',
+    'all_customer' : 'api/getallCustomers'
+}
+
+def main():
+    while True:
+        customer_amount = input("How many customers do you wish to create?: ")
+        try:
+            customer_amount = int(customer_amount)
+            for customer in range(customer_amount): #create required amount of customers
+                boolean = randint(0,1) #roll whether a business or a private customer should be generated
+                customer += 1 #add one to customers for a proper output
+                if boolean==0:
+                    cust_data = gen_customer("B")
+                    response = requests.post(URL + API['create_customer'], data = cust_data)
+                    print("Customer "+ str(customer) + " was created with response code: "+ str(response))
+                else:
+                    cust_data = gen_customer("C")
+                    response = requests.post(URL + API['create_customer'], data = cust_data)
+                    print("Customer "+ str(customer) + "was created with response code: "+ str(response))
+            break
+        except ValueError:
+            print("Please use (whole) numbers only.")
+
+    while True:
+        order_amount = input("How many orders do you wish to create (Only existing customers will be used)?: ")
+
+        response = requests.get(URL + API['all_customer'])
+        customers = json.loads(response.content)
+
+        try:
+            order_amount = int(order_amount)
+            for order in range(order_amount):
+                order += 1
+                random_customer = choice(customers)['kundennummer']
+                order_data = gen_order(random_customer)
+                print(order_data)
+                response = requests.post(URL + API['create_order'], data = json.dumps(order_data))
+                print("Order "+ str(order) + " was created with response code: "+ str(response))
+            break
+        except ValueError:
+            print("Please use numbers only.")
+
+    print("Job is done")
+
+main()
+
+
+
+
+
+
+
